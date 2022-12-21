e5158d553d457d80f9c7592b14a7b251ce3f0809
Bartosz <bartosz@akyb.int.pl>
2022-12-21 20:28:22 +0100

    6zmiana

--- app.js
+++ app.js
@@ -10,38 +10,43 @@ const budgetToDisplay = document.querySelector(".totalBudget");
 
 const listDisplay = document.querySelector(".list-item");
 
+
 const listItem = document.createElement("li");
-//1 dodać do tego elementu klasę taka jaką on ma w html
 listItem.setAttribute('class', 'list-item');
-//2 żeby móc dodać div i button trzeba go stworzyć w js
-const divElementName = document.createElement("div");
-divElementName.setAttribute('class', 'col-1');
-const divElementCost = document.createElement("div");
-divElementCost.setAttribute('class', 'col-2');
-const btnEdit = document.createElement("button");
-btnEdit.className = "btn-icon";   
-const btnDelete = document.createElement("button");
-btnDelete.className = "btn-icon";
 
-// w guziku na razie stworzyłeś sam button w środku button ma element i więc musisz stworzyć element html i dodać mu klasę
-
-const iconItem = document.createElement("i");
-iconItem.setAttribute('class', 'editIcon');
-document.body.appendChild(iconItem);
-
-// odnieść się do ul
 
 const tableList = document.getElementById("idTable");
-// tableList.appenChild(listItem);
+tableList.appendChild(listItem);
 
 
-// stworzyć elementy znajdujące się w li i je przypisać do
+const name1 = document.createElement("div");
+name1.setAttribute('class', 'col col-1');
+name1.setAttribute('data-label', "Product");
+listItem.appendChild(name1);
+
+const name2 = document.createElement("div");
+name2.setAttribute('class', 'col col-2');
+name2.setAttribute('data-label', "Cost");
+listItem.appendChild(name2);
+
+const btnEdit = document.createElement("button");
+btnEdit.setAttribute('class', 'btn-icon');
+btnEdit.setAttribute('type', 'button');
+listItem.appendChild(btnEdit);
+
+const btnDelete = document.createElement("button");
+btnDelete.setAttribute('class', 'btn-icon');
+btnDelete.setAttribute('type', 'button');
+listItem.appendChild(btnDelete);
 
 
+const icon1 = document.createElement("i");
+icon1.setAttribute('class', 'editIcon fa-solid fa-pen-to-square');
+btnEdit.appendChild(icon1);
 
-//3 dodac do tego elementu dzieci div x2 i button x2 
-
-
+const icon2 = document.createElement("i");
+icon2.setAttribute('class', 'deleteIcon fa-solid fa-trash');      
+btnDelete.appendChild(icon2);
 
 
 // lista 
@@ -49,15 +54,6 @@ const tableList = document.getElementById("idTable");
 let budgetValue = 0;
 let productName = "";
 let expensesValue = 0;
-
-// Expense List zmienna
-// const listItemsArray = [{name: 'col-1', value: 'col-2' }];
-
-
-
-
-
-
 
 btnSetBudget.addEventListener("click", () => {
     budgetValue = budgetInput.value;
@@ -73,10 +69,4 @@ btnCheckAmount.addEventListener("click", () => {
 });
 
 
-
-
-
-// Po kliknięciu w button "Set Budget" wartość z input powinna przenieść się do "Total Budget"
-
-// Po kliknięciu w button "Check Amount" wartość z input text i number powinna przenieść się do Tablicy "Expence List" która będzie miała wartość Tekstową i numeryczną z dodatkową opcją Edytuj i usuń.
 
