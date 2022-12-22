c87993e5f3b534aaab7275c4ee0539910d93797f
Bartosz <bartosz@akyb.int.pl>
2022-12-22 20:41:34 +0100

    7zmiana

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const btnSetBudget = document.querySelector(".setBudget");
 const btnCheckAmount = document.querySelector(".checkAmount");
 const budgetInput = document.querySelector(".inputAmount");
+
 const productNameInput = document.querySelector(".product");
 const productValueInput = document.querySelector(".costProduct");
 
 const budgetToDisplay = document.querySelector(".totalBudget");
-// const expensesToDisplay = document.querySelector(".totalExpenses");
+
+// const expensesNameToList = document.querySelector(".col-1");
 // const balanceToDisplay = document.querySelector(".balance");
 
 const listDisplay = document.querySelector(".list-item");
@@ -48,7 +50,16 @@ const icon2 = document.createElement("i");
 icon2.setAttribute('class', 'deleteIcon fa-solid fa-trash');      
 btnDelete.appendChild(icon2);
 
+// Funkcja
 
+// function showName1(index) {
+//     var li = document.createElement('li');
+//     li.setAttribute('class','listItem');
+
+//     ul.appendChild(li);
+
+//     li.innerHTML=li.innerHTML + element;
+// };
 // lista 
 
 let budgetValue = 0;
@@ -65,7 +76,7 @@ btnCheckAmount.addEventListener("click", () => {
     // console.log("kliknąłem na element B");
     productName = productNameInput.value;
     expensesValue = productValueInput.value;
-    listDisplay.innerHTML = listItemsArray;
+    listItem.innerHTML = productName, expensesValue;
 });
 
 
