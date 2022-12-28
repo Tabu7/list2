b9f7e3c05c7c1dc038d5486d2caec34186a09402
Bartosz <bartosz@akyb.int.pl>
2022-12-28 14:30:13 +0100

    7zmiana zrobiona funkcja dla li

--- app.js
+++ app.js
@@ -13,54 +13,72 @@ const budgetToDisplay = document.querySelector(".totalBudget");
 const listDisplay = document.querySelector(".list-item");
 
 
-const listItem = document.createElement("li");
-listItem.setAttribute('class', 'list-item');
-
-
-const tableList = document.getElementById("idTable");
-tableList.appendChild(listItem);
-
-
-const name1 = document.createElement("div");
-name1.setAttribute('class', 'col col-1');
-name1.setAttribute('data-label', "Product");
-listItem.appendChild(name1);
-
-const name2 = document.createElement("div");
-name2.setAttribute('class', 'col col-2');
-name2.setAttribute('data-label', "Cost");
-listItem.appendChild(name2);
-
-const btnEdit = document.createElement("button");
-btnEdit.setAttribute('class', 'btn-icon');
-btnEdit.setAttribute('type', 'button');
-listItem.appendChild(btnEdit);
-
-const btnDelete = document.createElement("button");
-btnDelete.setAttribute('class', 'btn-icon');
-btnDelete.setAttribute('type', 'button');
-listItem.appendChild(btnDelete);
-
-
-const icon1 = document.createElement("i");
-icon1.setAttribute('class', 'editIcon fa-solid fa-pen-to-square');
-btnEdit.appendChild(icon1);
-
-const icon2 = document.createElement("i");
-icon2.setAttribute('class', 'deleteIcon fa-solid fa-trash');      
-btnDelete.appendChild(icon2);
-
 // Funkcja
 
-// function showName1(index) {
-//     var li = document.createElement('li');
-//     li.setAttribute('class','listItem');
+// zrobić funkcje która zrobi całe li z dziećmi 
 
-//     ul.appendChild(li);
+// wrzucić do Funkcji rzeczy które się powtarzają czyli button edit i delete
 
-//     li.innerHTML=li.innerHTML + element;
-// };
-// lista 
+
+
+
+function createTable(col1, col2, showLi, button, showEditButton, showDeleteButton) {
+    const btnLi = document.createElement("li");
+    btnLi.setAttribute('class', showLi)
+
+    const name1 = document.createElement("div");
+    name1.setAttribute('class', col1);
+    name1.setAttribute('data-label', "Product");
+    btnLi.appendChild(name1);
+
+    const name2 = document.createElement("div");
+    name2.setAttribute('class', col2);
+    name2.setAttribute('data-label', "Cost");
+    btnLi.appendChild(name2);
+
+
+    const btnEdit = document.createElement("button");
+    btnEdit.setAttribute('class', 'btn-icon btn-icon-edit');
+    btnEdit.setAttribute('type', button);
+    btnLi.appendChild(btnEdit);
+
+    const btnDelete = document.createElement("button");
+    btnDelete.setAttribute('class', 'btn-icon btn-icon-delete');
+    btnDelete.setAttribute('type', button);
+    btnLi.appendChild(btnDelete);
+
+
+    const icon1 = document.createElement("i");
+    icon1.setAttribute('class', showEditButton);
+    btnEdit.appendChild(icon1);
+
+    const icon2 = document.createElement("i");
+    icon2.setAttribute('class', showDeleteButton);      
+    btnDelete.appendChild(icon2);
+
+
+    const tableList = document.getElementById("idTable");
+    tableList.setAttribute('class', 'expenseTable')
+    tableList.appendChild(btnLi);
+
+    // btnLi.innerHTML = "Click Me"
+    tableList.appendChild(btnLi);
+
+ 
+    
+}
+
+createTable('col col-1', 'col col-2', 'list-item','button', 'editIcon fa-solid fa-pen-to-square', 'deleteIcon fa-solid fa-trash'); 
+createTable('col col-1', 'col col-2', 'list-item','button', 'editIcon fa-solid fa-pen-to-square', 'deleteIcon fa-solid fa-trash'); 
+
+function clickButton() {
+    // alert('wcisnięto przycisk');
+     
+}
+btnCheckAmount.addEventListener('click', clickButton);
+
+
+// ROW 1
 
 let budgetValue = 0;
 let productName = "";
@@ -72,12 +90,13 @@ btnSetBudget.addEventListener("click", () => {
     budgetToDisplay.innerHTML = budgetValue;
 });
 
-btnCheckAmount.addEventListener("click", () => {
-    // console.log("kliknąłem na element B");
-    productName = productNameInput.value;
-    expensesValue = productValueInput.value;
-    listItem.innerHTML = productName, expensesValue;
-});
+// btnCheckAmount.addEventListener("click", () => {
+//     // console.log("kliknąłem na element B");
+//     productName = productNameInput.value;
+//     expensesValue = productValueInput.value;
+//     listItem.innerHTML = productName, expensesValue;   
+// });
+
 
 
 
